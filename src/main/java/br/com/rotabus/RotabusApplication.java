354ac87f3ea5cfaf8829dfe116384dd777c1b790
ThiagoIanuch <thiagoianuch@hotmail.com>
2026-04-20 21:09:05 -0300

    Adicionar estrutura base do projeto

---
+++ src/main/java/br/com/rotabus/RotabusApplication.java
@@ -0,0 +1,13 @@
+package br.com.rotabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotabusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotabusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rotabus/RotabusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rotabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotabusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
