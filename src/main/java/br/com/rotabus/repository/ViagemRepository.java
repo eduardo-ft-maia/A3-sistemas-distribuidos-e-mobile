d8a27bab515e4cd5555a0157276b44f864a3a8d2
Luan Borges <luanhborsilva@gmail.com>
2026-05-12 15:53:31 -0300

    Adiciona funcionalidade de busca de viagens

--- src/main/java/br/com/rotabus/repository/ViagemRepository.java
+++ src/main/java/br/com/rotabus/repository/ViagemRepository.java
@@ -3,5 +3,12 @@ package br.com.rotabus.repository;
 import br.com.rotabus.model.Viagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+
+    List<Viagem> findByCidadeOrigemNomeAndCidadeDestinoNome(
+            String origem,
+            String destino
+    );
 }
